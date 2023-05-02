9afc488343422f906558fd977c1bc21a1f41eb8b
Natalija <natalijaallahverdijeva@gmail.com>
2023-05-02 16:46:17 +0200

    Home Work 26.04.2023

---
+++ HW 28.04.2023/src/Account.java
@@ -0,0 +1,28 @@
+public class Account {
+    private String iban;
+    private double balance;
+    private Person person;
+
+    public Account(String iban, double balance, Person person) {
+        this.iban = iban;
+        this.balance = balance;
+        this.person = person;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    @Override
+    public String toString() {
+        return "IBAN: " + iban + " / " + balance;
+    }
+}
