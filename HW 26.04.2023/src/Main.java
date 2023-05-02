689f658e08feb181a2e4b8f82eb0259c231c87d4
Natalija <natalijaallahverdijeva@gmail.com>
2023-05-02 15:32:12 +0200

    Home Work 26.04.2023

--- HW 26.04.2023/src/Main.java
+++ HW 26.04.2023/src/Main.java
@@ -1,3 +1,10 @@
+//Дан список Programmer(String name, String city, List<Task>  tasks).
+// Каждый программист  имеет список задач Task (int Number, String description, String status, int daysInProcessing) .
+//Используя Stream сформировать лист из 5 задач которые дольше всего находятся в работе.
+
+
+
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -6,26 +13,42 @@ public class Main {
     public static void main(String[] args) {
 
         List<Programmer> programmers = List.of(
-                new Programmer("Pr.1", "Berlin", List.of(new Task(8, "testing", "in process", 2 ))),
-                new Programmer("Pr.2", "Oslo", List.of(new Task(3, "tests coding", "finished", 5 ))),
-                new Programmer("Pr.3", "Munich", List.of(new Task(1, "app develop", "in process", 30 ))),
-                new Programmer("Pr.4", "Berlin", List.of(new Task(2, "updating", "finished", 2 ))),
-                new Programmer("Pr.5", "Dubai", List.of(new Task(4, "data analise", "finished", 7 ))),
-                new Programmer("Pr.6", "Hamburg", List.of(new Task(5, "visualisation", "in process", 15 ))),
-                new Programmer("Pr.7", "Tallinn", List.of(new Task(6, "testing", "failed", 9 )))
+                new Programmer("Pr.1", "Berlin", List.of(new Task(8, "testing", "in process", 2))),
+                new Programmer("Pr.2", "Oslo", List.of(new Task(3, "tests coding", "finished", 5))),
+                new Programmer("Pr.3", "Munich", List.of(new Task(1, "app develop", "in process", 30))),
+                new Programmer("Pr.4", "Berlin", List.of(new Task(2, "updating", "finished", 2))),
+                new Programmer("Pr.5", "Dubai", List.of(new Task(4, "data analise", "finished", 7))),
+                new Programmer("Pr.6", "Hamburg", List.of(new Task(5, "visualisation", "in process", 15))),
+                new Programmer("Pr.7", "Tallinn", List.of(new Task(6, "testing", "failed", 9)))
 
         );
 
         List res = programmers.stream()
-                                .flatMap(programmer -> programmer.getTasks().stream())
-                                .sorted(Comparator.comparingInt(Task::getDaysInProcessing)
-                                    .reversed())
-                                .limit(5)
-                                .collect(Collectors.toList());
+                .flatMap(programmer -> programmer.getTasks().stream())
+                //.sorted(Comparator.comparingInt(Task::getDaysInProcessing) //sorted((t1,t2)->t2.g-t1)
+                // .reversed())
+                .sorted((t1, t2) -> t2.getDaysInProcessing() - t1.getDaysInProcessing())
+                .limit(5)
+                .collect(Collectors.toList());
 
         res.forEach(System.out::println);
 
 
-
     }
+
+//Дана строка вида ключ1:значение1;ключ2:значение2;ключ1:значение2;ключ3:значение1 .....
+//Написать метод, который возвращает последнее значение заданного ключа.
+//Например: "Jack:room 10;Nick:room 20;Jack:room 1;Mike:room3", "Jack" -> "room 1"
+//Если в строке нет заданного ключа возвращается пустая строка.
+
+        public static String valueFromString(String string, String key) {
+            if(string==null || key==null) return "";
+            return Arrays.stream(string.split(";"))
+                    .filter(s -> s.startsWith(key + ":"))
+                    .reduce((acc, s) -> s.substring(s.indexOf(':') + 1))
+                    .orElse("");
+        }
+
+
+
 }
--- HW 28.04.2023/src/Main.java
+++ HW 28.04.2023/src/Main.java
@@ -1,5 +1,32 @@
+//Дан список Account{Person person, String iban, double balance}.
+// Необходимо сформировать Map<Person, Account>, где ключом будет Person, а значением список его счетов.
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List <Account> accounts = List.of(
+                new Account("DE123456", 100.50, new Person("Adam", 20)),
+                new Account("DE654321", 110.00, new Person("Eve", 25)),
+                new Account("DE123123", 1000.00, new Person("Adam", 20)),
+                new Account("DE111111", 300.20, new Person("Adam", 20)),
+                new Account("DE456789", 538.20, new Person("Adam", 45))
+        );
+
+
+        Map<Person, List> map = accounts.stream()
+                .collect(Collectors.toMap(o -> o.getPerson(), o -> List.of(o.getIban()+": "+o.getBalance()),
+                        (o1, o2) -> (List.of(o1+","+o2))));
+        System.out.println(map);
+
+
+
+
     }
 }
